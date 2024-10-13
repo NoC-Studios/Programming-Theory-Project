1dccc4b1861ca61f83b4410b2c6581950f11afa8
Mike.Dev <42576728+MikeMeade@users.noreply.github.com>
2024-10-13 15:17:32 -0400

    Implemented pulsing for game pieces on collision
    Game pieces will "pulse" or "glow" with emissions when colliding with other objects.

--- Assets/NoC_Studios/GeoPhysX/Scripts/GamePiece.cs
+++ Assets/NoC_Studios/GeoPhysX/Scripts/GamePiece.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace NoC.Studios.GeoPhysX
@@ -7,6 +8,18 @@ namespace NoC.Studios.GeoPhysX
     /// </summary>
     public class GamePiece : MonoBehaviour
     {
+        /// <summary>
+        /// The shader property name used to control the emission color of a material. This constant string value is particularly
+        /// useful for dynamically changing or adjusting the emission color of materials in the game environment.
+        /// </summary>
+        const string k_emissionColor = "_EmissionColor";
+
+        /// <summary>
+        /// Stores the unique identifier for the shader property that controls the emission color of materials.
+        /// This identifier is crucial for setting or modifying the emission color dynamically within the game environment.
+        /// </summary>
+        static readonly int EmissionColor = Shader.PropertyToID(k_emissionColor);
+        
         /// <summary>
         /// The name of the material used for the red game piece. This constant string value
         /// is used to reference the red material in the game environment.
@@ -60,18 +73,147 @@ namespace NoC.Studios.GeoPhysX
         public PieceColor Color => m_pieceColor;
 
         /// <summary>
+        /// The color used to represent the glow effect of the game piece. This variable holds a UnityEngine.Color value
+        /// that determines the visual glow color of the game piece within the game environment.
+        /// </summary>
+        [SerializeField] Color m_glowColor = UnityEngine.Color.white;
+
+        /// <summary>
+        /// The duration of the pulse effect for the game piece. This constant floating-point value
+        /// specifies the length of time in seconds that the pulse effect remains active.
+        /// </summary>
+        const float k_pulseDuration = 1f;
+
+        /// <summary>
+        /// The intensity of the glow effect for the game piece. This float value ranges from 0 (no glow) to 10 (maximum glow),
+        /// allowing customization of how prominently the game piece emits light or stands out in the game environment.
+        /// </summary>
+        [Range(0f, 10f)] [SerializeField] float m_glowIntesity = 1f;
+
+        /// <summary>
+        /// The time interval in seconds during which the game piece pulses or changes its visual state. This variable is used to control or animate
+        /// the pulsing effect of the game piece, creating dynamic visual feedback based on the specified pulse time.
+        /// </summary>
+        float m_pulseTime;
+
+        /// <summary>
+        /// Indicates whether the game piece is currently pulsing. This boolean value determines if the game piece
+        /// is in an animated state that causes it to pulse visually, adding a dynamic effect within the game environment.
+        /// </summary>
+        bool m_isPulsing = false;
+
+        /// <summary>
+        /// The material associated with the game piece. This variable is used to hold
+        /// the Material object that defines the appearance of the game piece in terms
+        /// of texture, shader, and color within the game environment.
+        /// </summary>
+        Material m_material;
+
+        /// <summary>
+        /// Initializes the game piece by setting up its material component.
+        /// This method is called when the script instance is being loaded.
+        /// </summary>
+        void Awake()
+        {
+            m_material = GetComponentInChildren<Renderer>().material;
+        }
+
+        /// <summary>
+        /// Enables the emission keyword for the material associated with the game piece.
+        /// This method is called when the script instance is enabled just before any of the
+        /// Update methods are called for the first time.
+        /// </summary>
+        void Start()
+        {
+            const string emissionKeyword = "_EMISSION"; 
+            m_material.EnableKeyword(emissionKeyword);
+        }
+
+        /// <summary>
+        /// Updates the game piece's pulsing state on each frame refresh. This method handles the timing and transition for the pulsing
+        /// effect. If the game piece is pulsing, it increments the pulse time by delta time and triggers the Pulse method.
+        /// Once the pulse duration is met, the pulsing state is reset.
+        /// </summary>
+        void Update()
+        {
+            if (m_isPulsing)
+            {
+                m_pulseTime += Time.deltaTime;
+                Pulse();
+            }
+
+            if (m_pulseTime >= k_pulseDuration)
+            {
+                m_isPulsing = false;
+                ResetPulseTime();
+            }
+        }
+
+        /// <summary>
+        /// Begins the pulsing animation for the game piece. This method enables the pulsing state,
+        /// which is a visual effect to highlight the game piece within the game environment.
+        /// </summary>
+        void StartPulse()
+        {
+            m_isPulsing = true;
+            ResetPulseTime();
+        }
+
+        /// <summary>
+        /// Updates the emission color of the game piece material based on the current pulse time.
+        /// This method creates a pulsing visual effect by varying the intensity of the emission color.
+        /// It uses a PingPong function to calculate the pulsing effect proportionally within the pulse duration.
+        /// </summary>
+        void Pulse()
+        {
+            var emission = Mathf.PingPong(m_pulseTime, k_pulseDuration) / k_pulseDuration;
+            m_material.SetColor(EmissionColor, m_glowColor * emission * m_glowIntesity);
+        }
+
+        /// <summary>
+        /// Resets the pulse time to its initial value and updates the emission state of the game piece.
+        /// </summary>
+        void ResetPulseTime()
+        {
+            m_pulseTime = 0;
+            ResetEmission();
+        }
+
+        /// <summary>
+        /// Resets the emission color of the game piece's material to black.
+        /// This method is typically called to turn off any emission effects on the material,
+        /// ensuring that the game piece appears non-emissive.
+        /// </summary>
+        void ResetEmission()
+        {
+            m_material.SetColor(EmissionColor, UnityEngine.Color.black);
+        }
+
+        /// <summary>
         /// Sets the color of the game piece based on the specified material.
         /// </summary>
         /// <param name="color">The material whose color name determines the color of the game piece.</param>
         public void SetColor(Material color)
         {
-            m_pieceColor = color.name switch
+            switch (color.name)
             {
-                k_redColorMaterialName => PieceColor.Red,
-                k_greenColorMaterialName => PieceColor.Green,
-                k_blueColorMaterialName => PieceColor.Blue,
-                _ => PieceColor.None
-            };
+                case k_redColorMaterialName:
+                    m_pieceColor = PieceColor.Red;
+                    m_glowColor = UnityEngine.Color.red;
+                    break;
+                case k_greenColorMaterialName:
+                    m_pieceColor = PieceColor.Green;
+                    m_glowColor = UnityEngine.Color.green;
+                    break;
+                case k_blueColorMaterialName:
+                    m_pieceColor = PieceColor.Blue;
+                    m_glowColor = UnityEngine.Color.blue;
+                    break;
+                default:
+                    m_pieceColor = PieceColor.None;
+                    m_glowColor = UnityEngine.Color.white;
+                    break;
+            }
         }
 
         /// <summary>
@@ -82,6 +224,7 @@ namespace NoC.Studios.GeoPhysX
         void OnCollisionEnter(Collision other)
         {
             GameManager.Instance.PlayCollisionSound();
+            StartPulse();
         }
     }
 }
